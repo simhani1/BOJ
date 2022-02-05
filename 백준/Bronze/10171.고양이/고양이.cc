948f8de9219b654608f217464f570e7ebbce6b0a
simhani1 <48800281+simhani1@users.noreply.github.com>
2022-02-06 04:07:46 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171.고양이/고양이.cc
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	printf("\\    /\\\n");
+	printf(" )  ( ')\n");
+	printf("(  /  )\n");
+	printf(" \\(__)|\n");
+	return 0;
+}
