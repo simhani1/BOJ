d3b3c90f1808ec33dff003dc148e26d3c2cafe03
Jonghan Sim <48800281+simhani1@users.noreply.github.com>
2022-02-08 23:45:47 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718.We love kriii/We love kriii.cc
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+
+int main(void) {
+    printf("강한친구 대한육군\n강한친구 대한육군");
+    return 0;
+}
