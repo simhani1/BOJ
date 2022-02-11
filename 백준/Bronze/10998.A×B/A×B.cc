619df9411129f37a58e62177fd41eb5807ed9734
Jonghan Sim <48800281+simhani1@users.noreply.github.com>
2022-02-12 00:48:23 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998.A×B/A×B.cc
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <time.h>
+
+using namespace std;
+
+int main(void) {
+    int A, B;
+    cin >> A >> B;
+    cout << A * B;
+    return 0;
+}
