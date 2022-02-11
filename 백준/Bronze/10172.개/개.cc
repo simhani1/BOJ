79f51069cc5180fdadb997cee6c36838fbf44995
Jonghan Sim <48800281+simhani1@users.noreply.github.com>
2022-02-12 00:44:39 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172.개/개.cc
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <time.h>
+
+using namespace std;
+
+int main(void) {
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"\`    |\n");
+    printf("||_/=\\\\__|\n");
+    return 0;
+}
