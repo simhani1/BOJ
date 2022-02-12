7a90d14cb27777d55359c01562b46c9befd249f0
Jonghan Sim <48800281+simhani1@users.noreply.github.com>
2022-02-12 22:36:51 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108.1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <time.h>
+
+using namespace std;
+
+int main(void) {
+    int y;
+    cin >> y;
+    cout << y - 543;
+    return 0;
+}
