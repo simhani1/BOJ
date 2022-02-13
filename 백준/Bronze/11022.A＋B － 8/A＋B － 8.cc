1727c208d57f7d9ef6fe11d43fed99efe18480bd
Jonghan Sim <48800281+simhani1@users.noreply.github.com>
2022-02-13 16:56:34 +0900

    [Bronze III] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022.A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <time.h>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int T, A, B;
+    cin >> T;
+    for (int i = 1; i <= T; i++) {
+        cin >> A >> B;
+        cout << "Case #" << i << ": " << A << " + " << B << " = " << A + B << '\n';
+    }
+    return 0;
+}
