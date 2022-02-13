a1b93c708b19b78013cb09316d5c4f9ed2ed401f
Jonghan Sim <48800281+simhani1@users.noreply.github.com>
2022-02-13 16:53:41 +0900

    [Bronze III] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741.N 찍기/N 찍기.cc
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <time.h>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cout << i << '\n';
+    }
+    return 0;
+}
