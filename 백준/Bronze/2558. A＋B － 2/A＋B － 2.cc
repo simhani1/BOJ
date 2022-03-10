5b2316c5b355eed334361ca6f3f2b7a567de9fd7
Jonghan Sim <48800281+simhani1@users.noreply.github.com>
2022-03-10 22:43:52 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int A, B;
+    cin >> A >> B;
+    cout << A + B;
+    return 0;
+}
