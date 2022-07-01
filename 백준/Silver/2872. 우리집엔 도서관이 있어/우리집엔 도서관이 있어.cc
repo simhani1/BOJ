d0e8c54ea26801bd6b298885429218f1bf8e0ba3
Jonghan Sim <48800281+simhani1@users.noreply.github.com>
2022-07-02 05:01:18 +0900

    [Silver II] Title: 우리집엔 도서관이 있어, Time: 32 ms, Memory: 5220 KB -BaekjoonHub

---
+++ 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.cc
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int N, M;
+vector<int> v;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> M;
+        v.push_back(M);
+    }
+    for (int i = v.size() - 1; i >= 0; i--)
+    {
+        if (v[i] == N)
+            N--;
+    }
+    cout << N;
+    return 0;
+}
