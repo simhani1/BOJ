7c42ee532a947578e89e37f35b06891e958675e9
Jonghan Sim <48800281+simhani1@users.noreply.github.com>
2022-07-08 05:47:39 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int N;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    for (int i = 0; i < N / 4; i++)
+    {
+        cout << "long ";
+    }
+    cout << "int";
+    return 0;
+}
