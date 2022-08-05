0c0311873cd7210083adc24f7d0863a0ce7c51d6
Jonghan Sim <48800281+simhani1@users.noreply.github.com>
2022-08-05 09:59:03 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,27 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
+#include <memory.h>
 #include <algorithm>
 #include <vector>
 #include <cmath>
-
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+#include <map>
+#include <set>
 using namespace std;
 
+int N;
 int arr[500][500];
 
-int main(void) {
-	int N;
-	scanf("%d", &N);
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j <= i; j++) {
-			scanf("%d", &arr[i][j]);
-		}
-	}
-	for (int i = N - 1; i >= 0; i--) {
-		for (int j = 0; j < i; j++) {
-			arr[i - 1][j] = max(arr[i - 1][j] + arr[i][j], arr[i - 1][j] + arr[i][j + 1]);
-		}
-	}
-	printf("%d", arr[0][0]);
-	return 0;
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = N - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            arr[i - 1][j] += max(arr[i][j], arr[i][j + 1]); // 마지막 둘째 줄부터 시작해서 아래쪽 vs 우하단 중 큰 수를 골라 합해 나간다
+        }
+    }
+    cout << arr[0][0];
+    return 0;
 }
