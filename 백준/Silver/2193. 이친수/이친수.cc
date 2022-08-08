1619266a156b68bedafea63977ec58dd56316211
Jonghan Sim <48800281+simhani1@users.noreply.github.com>
2022-08-08 11:21:29 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.cc
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -1,32 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
+#include <memory.h>
 #include <algorithm>
 #include <vector>
 #include <cmath>
-
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+#include <map>
+#include <set>
 using namespace std;
 
 int N;
-long long dp[90] = { 1,1 };
+long long dp[91];
 
-long long pinary_num(int N) {
-	if (N - 1 < 2)
-		return dp[N - 1];
-	else {
-		for (int i = 2; i < N; i++) {
-			dp[i] = dp[i - 1] + dp[i - 2];
-		}
-	}
-	return dp[N - 1];
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    dp[1] = 1;
+    dp[2] = 1;
+    for (int i = 3; i <= N; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    cout << dp[N];
+    return 0;
 }
-int main(void) {
-	scanf("%d", &N);
-	printf("%lld", pinary_num(N));
-	return 0;
-}
-
-//바로 전이 0이라면 0 또는 1추가
-//바로 전이 1이라면 0추가
-//dp[N]에 n자리 이친수의 개수를 저장
-//규칙은 피보나치 수열과 동일함을 발견
