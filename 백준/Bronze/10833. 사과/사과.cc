f270c598adcee9d63dc7a4baa4850806376bc0ab
Jonghan Sim <simhani1@gmail.com>
2022-08-20 02:55:26 +0900

    [Bronze III] Title: 사과, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.cc
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int N, A, B;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    int ans = 0;
+    while (N--)
+    {
+        cin >> A >> B;
+        ans += B % A;
+    }
+    cout << ans;
+    return 0;
+}
