534dc5487b51028dfeef38a7c74118ee4b7f4134
Jonghan Sim <simhani1@gmail.com>
2022-08-20 03:27:02 +0900

    [Bronze III] Title: 삼각 무늬 - 2, Time: 72 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11312. 삼각 무늬 － 2/삼각 무늬 － 2.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+#include <map>
+#include <set>
+
+using namespace std;
+
+long long T, A, B;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> T;
+    while (T--)
+    {
+        cin >> A >> B;
+        A *= A;
+        B *= B;
+        if (A % B)
+            cout << A / B + 1 << endl;
+        else
+            cout << A / B << endl;
+    }
+    return 0;
+}
