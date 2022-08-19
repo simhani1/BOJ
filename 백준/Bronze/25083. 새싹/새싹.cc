54074958324952c0f7d10ca751a08253e33d158b
Jonghan Sim <simhani1@gmail.com>
2022-08-20 04:30:44 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int A, B;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cout << "         ,r\'\"7\n";
+    cout << "r`-_   ,'  ,/\n";
+    cout << " \\. \". L_r'\n";
+    cout << "   `~\\/\n";
+    cout << "      |\n";
+    cout << "      |\n";
+    return 0;
+}
