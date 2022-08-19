86b4855b1bdfee9a1f15178080f85880f67ecb88
Jonghan Sim <simhani1@gmail.com>
2022-08-20 04:36:40 +0900

    [Bronze V] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int N, M, sum = 0;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N >> M;
+    while (M--)
+    {
+        int A, B;
+        cin >> A >> B;
+        sum += A * B;
+    }
+    if (N == sum)
+        cout << "Yes";
+    else
+        cout << "No";
+    return 0;
+}
