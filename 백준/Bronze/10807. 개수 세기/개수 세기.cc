3fb08f41d920a9b619085f9797681f23e17584fb
Jonghan Sim <simhani1@gmail.com>
2022-08-20 03:07:50 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int N;
+int arr[201];
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    while (N--)
+    {
+        int M;
+        cin >> M;
+        arr[M + 100]++;
+    }
+    cin >> N;
+    cout << arr[N + 100];
+    return 0;
+}
