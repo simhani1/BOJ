41b062dfd1cece1f0ffe71be6adbfbc78e8b4ce3
Jonghan Sim <simhani1@gmail.com>
2022-08-21 00:55:01 +0900

    [Bronze III] Title: 지능형 기차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.cc
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int N, M, ans = 0;
+int arr[5];
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    for (int i = 1; i < 5; i++)
+    {
+        cin >> N >> M;
+        arr[i] = arr[i - 1];
+        arr[i] -= N;
+        arr[i] += M;
+        ans = max(ans, arr[i]);
+    }
+    cout << ans;
+    return 0;
+}
