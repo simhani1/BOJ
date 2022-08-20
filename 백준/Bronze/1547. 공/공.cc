a922537e48a6b562381e0326a3e644d0fd0d6980
Jonghan Sim <simhani1@gmail.com>
2022-08-21 00:16:59 +0900

    [Bronze III] Title: 공, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.cc
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int N, A, B, ball = 1;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A >> B;
+        if (ball == A)
+            ball = B;
+        else if (ball == B)
+            ball = A;
+    }
+    cout << ball;
+    return 0;
+}
