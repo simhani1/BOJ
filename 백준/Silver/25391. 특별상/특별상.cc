ec482c9bdc7617cb6bb06395ac0dd9afefd6747d
Jonghan Sim <simhani1@gmail.com>
2022-10-06 03:19:45 +0900

    [Silver I] Title: 특별상, Time: 392 ms, Memory: 983964 KB -BaekjoonHub

---
+++ 백준/Silver/25391. 특별상/특별상.cc
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+#include <map>
+#include <set>
+using namespace std;
+
+int N, M, K;
+long long sum = 0;
+priority_queue<pair<int,int>> pqA;
+priority_queue<pair<int,int>> pqB;
+bool visited[1000000001];
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N >> M >> K;
+    for(int i=0,A,B;i<N;i++){
+        cin>>A>>B;
+        pqA.push({A,B});
+        pqB.push({B,A});
+    }
+    for(int i=0;i<K;i++){
+        sum+=pqB.top().second;
+        visited[pqB.top().second]=true;
+        pqB.pop();
+    }
+    for(int i=0;i<M;i++){
+        if(!visited[pqA.top().first]){
+            sum+=pqA.top().first;
+            pqA.pop();
+        }
+        else{
+            pqA.pop();
+            i--;
+        }
+    }
+    cout<<sum;
+    return 0;
+}
