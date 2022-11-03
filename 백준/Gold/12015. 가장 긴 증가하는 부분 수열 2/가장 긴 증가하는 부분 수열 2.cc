096aa10983816254f6a7f529540eef934b751bf1
Jonghan Sim <simhani1@gmail.com>
2022-11-03 11:06:19 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 160 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+#include <map>
+#include <set>
+using namespace std;
+
+int N;
+int arr[1000001];
+int lis[1000001];
+
+int binarySearch(int l, int r, int find)
+{
+    while (l < r)
+    {
+        int mid = (l + r) / 2;
+        if (lis[mid] < find)
+            l = mid + 1;
+        else
+            r = mid;
+    }
+    return r;
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+    int i = 1, j = 0;
+    lis[0] = arr[0];
+    while (i < N)
+    {
+        if (lis[j] < arr[i])
+            lis[++j] = arr[i];
+        else
+        {
+            int idx = binarySearch(0, j, arr[i]);
+            lis[idx] = arr[i];
+        }
+        i++;
+    }
+    cout << j + 1 << endl;
+    return 0;
+}
