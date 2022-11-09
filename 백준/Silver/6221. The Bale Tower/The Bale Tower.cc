3d936db151f467ddaf5746b19b4ee4819514e012
Jonghan Sim <simhani1@gmail.com>
2022-11-09 19:14:10 +0900

    [Silver III] Title: The Bale Tower, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/6221. The Bale Tower/The Bale Tower.cc
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+#include <map>
+#include <set>
+using namespace std;
+
+int N, ans = 0;
+vector<pair<int, int>> v;
+int dp[21];
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    for (int i = 0, A, B; i < N; i++)
+    {
+        cin >> A >> B;
+        v.push_back({A, B});
+    }
+    sort(v.begin(), v.end());
+    for (int i = 0; i < N; i++)
+    {
+        dp[i] = 1;
+        for (int j = 0; j < i; j++)
+        {
+            if (v[i].second > v[j].second)
+                dp[i] = max(dp[i], dp[j] + 1);
+        }
+        ans = max(ans, dp[i]);
+    }
+    cout << ans << endl;
+    return 0;
+}
