a3fe06ac217b397541cea9668cdc722b5722002b
Jonghan Sim <simhani1@gmail.com>
2022-11-24 01:58:50 +0900

    [Silver III] Title: 단순한 문제 (Large), Time: 232 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25487. 단순한 문제 （Large）/단순한 문제 （Large）.cc
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <memory.h>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <time.h>
+#include <map>
+#include <set>
+using namespace std;
+
+int N;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    for (int i = 0, A, B, C; i < N; i++)
+    {
+        cin >> A >> B >> C;
+        cout << min(A, min(B, C)) << "\n";
+    }
+    return 0;
+}
