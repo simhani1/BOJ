75e78d51802987ea22d6e0e07a20122b958875aa
Jonghan Sim <simhani1@gmail.com>
2023-01-17 03:02:04 +0900

    [Gold I] Title: 구간 합 구하기, Time: 260 ms, Memory: 26224 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -11,22 +11,19 @@
 #include <time.h>
 #include <map>
 #include <set>
-
 using namespace std;
+#define INF 987654321
+#define INIT 100000000
 typedef long long ll;
 
 int N, M, K, H = 1;
 ll A, B, C;
-vector<ll> tree;
-vector<ll> num;
+vector<ll> v, tree;
 
 void update(int idx, ll value)
 {
-    // 단말 노드를 저장한 num배열의 인덱스값이 넘어오기 때문에 포화이진트리에서의 단말노드 번호로 바꿔준는 과정
-    // num배열의 0번째 원소 -> 첫번째 단말노드 -> 8번째 노드이므로 높이 H 값을 더해주면 된다
     idx += H;
     tree[idx] = value;
-    // 루트노드까지 값들을 갱신시켜준다.
     while (idx > 1)
     {
         idx /= 2;
@@ -34,14 +31,14 @@ void update(int idx, ll value)
     }
 }
 
-ll query(int L, int R, int node, int nodeL, int nodeR) // 찾는 범위 왼쪽 값, 찾는 범위 오른쪽 값, 해당 노드 번호, 해당 노드의 왼쪽 범위, 해당 노드의 오른쪽 범위
+ll query(int L, int R, int node, int nodeL, int nodeR)
 {
-    if (L <= nodeL && nodeR <= R) // 원하는 값을 노드에서 포함하고 있는 경우
+    if (L <= nodeL && nodeR <= R)
         return tree[node];
-    if (nodeR < L || R < nodeL) // 원하는 범위를 노드에서 포함하고 있지 않은 경우
+    if (nodeR < L || R < nodeL)
         return 0;
     int mid = (nodeL + nodeR) / 2;
-    return query(L, R, node * 2, nodeL, mid) + query(L, R, node * 2 + 1, mid + 1, nodeR); // 현재 노드에서 원하는 값을 찾지 못했으므로 자식 노드로 탐색을 진행
+    return query(L, R, node * 2, nodeL, mid) + query(L, R, node * 2 + 1, mid + 1, nodeR);
 }
 
 int main(void)
@@ -50,17 +47,14 @@ int main(void)
     cin.tie(0);
     cout.tie(0);
     cin >> N >> M >> K;
-    // 단말 노드의 개수 구하기
     while (N > H)
-    {
-        H *= 2;
-    }
-    tree.resize(H * 2 + 1);
-    num.resize(N + 1);
+        H *= 2; // 단말노드: h개 -> 전체 노드의 개수: 2*h-1
+    v.resize(N + 1);
+    tree.resize(2 * H + 1);
     for (int i = 0; i < N; i++)
     {
-        cin >> num[i];
-        update(i, num[i]);
+        cin >> v[i];
+        update(i, v[i]);
     }
     for (int i = 0; i < M + K; i++)
     {
