f10f7773496f29f1b316097beaf3b3a25e368b3d
Jonghan Sim <simhani1@gmail.com>
2023-02-22 03:00:40 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,11 +1,2 @@
 -- 코드를 입력하세요
-select
-    ao.animal_id as animal_id,
-    ao.animal_type as animal_type,
-    ao.name as name
-from animal_outs as ao
-    left join animal_ins as ai on ai.animal_id = ao.animal_id
-where 
-    ai.sex_upon_intake regexp 'Intact' and 
-    ao.sex_upon_outcome regexp 'Spayed|Neutered'
-order by animal_id
+SELECT
