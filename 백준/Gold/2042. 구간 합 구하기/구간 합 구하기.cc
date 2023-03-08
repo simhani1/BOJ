a242ade1ddacc983b673dde7cda0506f131ad5d2
Jonghan Sim <simhani1@gmail.com>
2023-03-09 02:32:25 +0900

    [Gold I] Title: 구간 합 구하기, Time: 228 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -4,41 +4,36 @@
 #include <memory.h>
 #include <algorithm>
 #include <vector>
-#include <cmath>
 #include <stack>
 #include <queue>
-#include <deque>
-#include <time.h>
-#include <map>
-#include <set>
+#include <cmath>
+
 using namespace std;
-#define INF 987654321
-#define INIT 100000000
+#define INF 2147483648
 typedef long long ll;
 
-int N, M, K, H = 1;
-ll A, B, C;
-vector<ll> v, tree;
+int N, M, K;
+ll arr[1000001];
+vector<ll> tree;
 
-void update(int idx, ll value)
+void update(ll idx, ll value)
 {
-    idx += H;
-    tree[idx] = value;
-    while (idx > 1)
+    while (idx < tree.size())
     {
-        idx /= 2;
-        tree[idx] = tree[idx * 2] + tree[idx * 2 + 1];
+        tree[idx] += value;
+        idx += (idx & -idx);
     }
 }
 
-ll query(int L, int R, int node, int nodeL, int nodeR)
+ll sum(ll idx)
 {
-    if (L <= nodeL && nodeR <= R)
-        return tree[node];
-    if (nodeR < L || R < nodeL)
-        return 0;
-    int mid = (nodeL + nodeR) / 2;
-    return query(L, R, node * 2, nodeL, mid) + query(L, R, node * 2 + 1, mid + 1, nodeR);
+    ll result = 0;
+    while (idx > 0)
+    {
+        result += tree[idx];
+        idx -= (idx & -idx);
+    }
+    return result;
 }
 
 int main(void)
@@ -47,22 +42,23 @@ int main(void)
     cin.tie(0);
     cout.tie(0);
     cin >> N >> M >> K;
-    while (N > H)
-        H *= 2; // 단말노드: h개 -> 전체 노드의 개수: 2*h-1
-    v.resize(N + 1);
-    tree.resize(2 * H + 1);
-    for (int i = 0; i < N; i++)
+    tree.resize(N + 1);
+    for (int i = 1; i <= N; i++)
     {
-        cin >> v[i];
-        update(i, v[i]);
+        cin >> arr[i];
+        update(i, arr[i]);
     }
     for (int i = 0; i < M + K; i++)
     {
+        ll A, B, C;
         cin >> A >> B >> C;
         if (A == 1)
-            update(B - 1, C);
+        {
+            update(B, C - arr[B]);
+            arr[B] = C;
+        }
         else
-            cout << query(B, C, 1, 1, H) << "\n";
+            cout << sum(C) - sum(B - 1) << "\n";
     }
     return 0;
 }
