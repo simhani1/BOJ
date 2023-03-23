aed713ba16bc5b3490a5ba92daa471bf4b96d9af
Jonghan Sim <simhani1@gmail.com>
2023-03-23 20:50:57 +0900

    [Platinum IV] Title: 단절점, Time: 24 ms, Memory: 4340 KB -BaekjoonHub

--- 백준/Platinum/11266. 단절점/단절점.cc
+++ 백준/Platinum/11266. 단절점/단절점.cc
@@ -4,42 +4,41 @@
 #include <memory.h>
 #include <algorithm>
 #include <vector>
-#include <cmath>
 #include <stack>
 #include <queue>
-#include <deque>
-#include <time.h>
-#include <map>
-#include <set>
+#include <cmath>
+
 using namespace std;
+#define INF 123456789
+typedef long long ll;
 
-int V, E, seq, cnt = 0;
+int V, E, seq = 0, ans = 0;
 int discovered[10001]; // DFS 탐색 순서 저장
-bool point[10001];     // 단절점 여부 저장
-vector<int> adj[10001];
+bool point[10001];
+vector<int> edge[10001];
 
-int dfs(int A, bool isRoot)
+int dfs(int now, int isRoot)
 {
-    discovered[A] = ++seq;
-    int ret = discovered[A]; // 인접 노드 중 방문시간이 제일 빠른 노드를 저장
-    int child = 0;           // A가 루트노드인 경우 자식 수를 확인
-    for (int i = 0; i < adj[A].size(); i++)
+    discovered[now] = ++seq;
+    int ret = discovered[now]; // 인접 노드들의 노드 중 도달 가능한 제일 높은 높이 저장
+    int child = 0;             // now가 루트노드인 경우 자식 수가 2 이상인지 확인하기 위한 용도
+    for (int i = 0; i < edge[now].size(); i++)
     {
-        int nextNode = adj[A][i];
-        if (!discovered[nextNode])
+        int nextNode = edge[now][i];
+        if (!discovered[nextNode]) // 다음 노드를 아직 방문하지 않은 경우
         {
             child++;
-            int low = dfs(nextNode, false);      // low: 정점 A의 자식 노드가 갈 수 있는 노드 중, 제일 빨리 방문한 노드
-            if (!isRoot && low >= discovered[A]) // 루트가 아닌 현재 노드의 방문 순서보다 자식 노드의 방문 순서가 더 크거나 같다면 단절점
-                point[A] = true;
+            int low = dfs(nextNode, false);        // 현재 노드의 자식 노드가 방문가능한 노드의 최고 도달 깊이
+            if (!isRoot && low >= discovered[now]) // 루트노드가 아니고 자식노드가 현재 노드보다 더 높은 높이에 도달 불가능한 경우
+                point[now] = true;
             ret = min(ret, low);
         }
         else
-            ret = min(ret, discovered[nextNode]); // 이미 방문했다면, 현재 정점의 방문 순서와 탐색된 정점의 방문 순서 중 작은값을 찾는다.
+            ret = min(ret, discovered[nextNode]);
     }
-    if (isRoot && child >= 2)
-        point[A] = true;
-    return ret; // A 자식노드가 A를 거치지 않고 도달할 수 있는 정점 중, 제일 먼저 DFS함수가 방문한 정점
+    if (isRoot && 2 <= child)
+        point[now] = true;
+    return ret;
 }
 
 int main(void)
@@ -51,8 +50,8 @@ int main(void)
     for (int i = 0, A, B; i < E; i++)
     {
         cin >> A >> B;
-        adj[A].push_back(B);
-        adj[B].push_back(A);
+        edge[A].push_back(B);
+        edge[B].push_back(A);
     }
     for (int i = 1; i <= V; i++)
     {
@@ -62,9 +61,9 @@ int main(void)
     for (int i = 1; i <= V; i++)
     {
         if (point[i])
-            cnt++;
+            ans++;
     }
-    cout << cnt << endl;
+    cout << ans << "\n";
     for (int i = 1; i <= V; i++)
     {
         if (point[i])
