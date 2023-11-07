d1ecc9f4b3a4df6a0b0ee06a10a3f784492353a0
Jonghan Sim <simhani1@gmail.com>
2023-11-08 02:54:22 +0900

    [Gold IV] Title: 서울에서 경산까지, Time: 364 ms, Memory: 57696 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.java
+++ 백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.java
@@ -11,7 +11,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N + 10][4];
+        int[][] arr = new int[N + 1][4];
         for (int i = 1; i <= N; i++) {
             String[] str = br.readLine().split(" ");
             for (int j = 0; j < str.length; j++) {
@@ -23,13 +23,29 @@ public class Main {
 
     private static int solve(int N, int K, int[][] arr) {
         int ans = 0;
-        int[][] dp = new int[101][100001];
+        int[][] dp = new int[N + 1][K + 1];
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= K; j++) {
+                dp[i][j] = -1;
+            }
+        }
         // init
-        dp[1][arr[1][0]] = arr[1][1];
-        dp[1][arr[1][2]] = arr[1][3];
+        /*
+        첫 도시를 가는데 걸리는 시간이 동일한 경우 예외 처리
+        2 4
+        2 5 2 2
+        2 2 2 2
+        */
+        if (arr[1][0] == arr[1][2]) {
+            dp[1][arr[1][0]] = Math.max(arr[1][1], arr[1][3]);
+        } else {
+            dp[1][arr[1][2]] = arr[1][3];
+            dp[1][arr[1][0]] = arr[1][1];
+        }
+        // solve
         for (int i = 2; i <= N; i++) {
             for (int j = 0; j <= K; j++) {
-                if (dp[i - 1][j] == 0)
+                if (dp[i - 1][j] == -1)
                     continue;
                 if (j + arr[i][0] <= K) {
                     dp[i][j + arr[i][0]] = Math.max(dp[i][j + arr[i][0]], dp[i - 1][j] + arr[i][1]);
