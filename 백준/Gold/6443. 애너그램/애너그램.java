11db8c984cd7501f1811562da9b8453ce755e9aa
Jonghan Sim <simhani1@gmail.com>
2024-01-30 01:46:33 +0900

    [Gold V] Title: 애너그램, Time: 408 ms, Memory: 58516 KB -BaekjoonHub

--- 백준/Gold/6443. 애너그램/애너그램.java
+++ 백준/Gold/6443. 애너그램/애너그램.java
@@ -1,15 +1,12 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.SortedSet;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 public class Main {
 
     private static BufferedReader br;
     private static StringTokenizer st;
     private static StringBuilder sb = new StringBuilder();
-    private static SortedSet<String> set = new TreeSet<>();
     private static int N, M;
     private static int[] visited;
 
@@ -17,7 +14,6 @@ public class Main {
         br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         for (int i = 0; i < N; i++) {
-            set.clear();
             String str = br.readLine();
             M = str.length();
             visited = new int[26];
@@ -26,16 +22,13 @@ public class Main {
                 visited[str.charAt(j) - 'a']++;
             }
             solve(str, "");
-            for (String s : set) {
-                sb.append(s + "\n");
-            }
         }
         System.out.println(sb.toString());
     }
 
     private static void solve(String str, String target) {
         if (target.length() == M) {
-            set.add(target);
+            sb.append(target).append("\n");
             return;
         }
         for (int i = 0; i < 26; i++) {
