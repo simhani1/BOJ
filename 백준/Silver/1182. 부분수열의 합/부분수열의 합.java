99ec958a081bde655365e0d2153839e0ec9db8b0
Jonghan Sim <simhani1@gmail.com>
2024-02-01 09:46:33 +0900

    [Silver II] Title: 부분수열의 합, Time: 196 ms, Memory: 15032 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,46 +1,47 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, S, ans;
-	static int[] arr;
-	static int flag = 0;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+    private static int N, S, ans;
+    private static int[] arr = new int[21];
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		S = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
-		arr = new int[N];
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		ans = 0;
-		method(0, 0);
-		System.out.println(ans);
-	}
+    public static void main(String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        ans = 0;
+        solve(0, 0);
+        System.out.println(ans);
+    }
 
-	private static void method(int flag, int cnt) {
-
-		if (cnt == N) {
-			if (flag == 0)
-				return;
-			int sum = 0;
-			for (int i = 0; i < N; i++) {
-				if (((1 << i) & flag) > 0) {
-					sum += arr[i];
-				}
-			}
-			if (sum == S)
-				ans++;
-			return;
-		}
-		method(flag, cnt + 1);
-		flag = (1 << cnt) | flag;
-		method(flag, cnt + 1);
-	}
+    private static void solve(int flag, int cnt) {
+        if (cnt == N) {
+            if (flag == 0) {
+                return;
+            }
+            int sum = 0;
+            for (int i = 0; i < N; i++) {
+                if (((1 << i) & flag) > 0) {
+                    sum += arr[i];
+                }
+            }
+            if (sum == S) {
+                ans++;
+            }
+            return;
+        }
+        solve(flag, cnt + 1);
+        flag = (1 << cnt) | flag;
+        solve(flag, cnt + 1);
+    }
 }
