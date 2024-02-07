5e601669162492ab467caba19039506ada0a99ac
Jonghan Sim <simhani1@gmail.com>
2024-02-07 11:25:50 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 
@@ -14,9 +12,6 @@ public class Main {
     private static final int PAPER_SIZE = 10;
     private static final int MAX_SIZE = 100;
     private static int[][] arr = new int[110][110];
-    private static boolean[][] visited = new boolean[110][110];
-    private static int[] dx = {0, 0, -1, 1};
-    private static int[] dy = {1, -1, 0, 0};
 
     public static void main(String[] args) throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
@@ -45,47 +40,10 @@ public class Main {
         for (int i = 0; i <= MAX_SIZE; i++) {
             for (int j = 0; j <= MAX_SIZE; j++) {
                 if (arr[i][j] != 0) {
-                    ans += bfs(i, j);
+                    ans++;
                 }
             }
         }
         System.out.println(ans);
-    }
-
-    private static int bfs(int x, int y) {
-        int cnt = 0;
-        Queue<Node> q = new ArrayDeque<>();
-        q.offer(new Node(x, y));
-        while (!q.isEmpty()) {
-            Node node = q.poll();
-            int nowX = node.x;
-            int nowY = node.y;
-            if (visited[nowX][nowY]) {
-                continue;
-            }
-            visited[nowX][nowY] = true;
-            cnt++;
-            for (int i = 0; i < 4; i++) {
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
-                if (nextX < 0 || nextX > MAX_SIZE || nextY < 0 || nextY > MAX_SIZE) {
-                    continue;
-                }
-                if (arr[nextX][nextY] > 0) {
-                    q.offer(new Node(nextX, nextY));
-                }
-            }
-        }
-        return cnt;
-    }
-}
-
-class Node {
-    int x;
-    int y;
-
-    public Node(int x, int y) {
-        this.x = x;
-        this.y = y;
     }
 }
