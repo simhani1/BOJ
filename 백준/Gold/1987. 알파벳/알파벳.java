e930c362d0219e76cc3c4b0eed7e5a23fbf4c0b1
Jonghan Sim <simhani1@gmail.com>
2024-02-20 11:57:12 +0900

    [Gold IV] Title: 알파벳, Time: 160 ms, Memory: 15084 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -11,7 +11,7 @@ public class Main {
     private static char[][] arr = new char[21][];
     private static int[] dx = {0, 0, -1, 1};
     private static int[] dy = {1, -1, 0, 0};
-    private static boolean[] visited = new boolean[27];
+    private static int[][] visited = new int[21][21];
 
     public static void main(String[] args) throws Exception {
         st = new StringTokenizer(br.readLine());
@@ -20,24 +20,31 @@ public class Main {
         for (int i = 0; i < R; i++) {
             arr[i] = br.readLine().toCharArray();
         }
-        dfs(0, 0, 1);
+        dfs(0, 0, 1, 1 << arr[0][0] - 'A');
         System.out.println(ans);
     }
 
-    private static void dfs(int nowX, int nowY, int cnt) {
-        visited[arr[nowX][nowY] - 'A'] = true;
+    private static void dfs(int nowX, int nowY, int cnt, int bit) {
         ans = Math.max(ans, cnt);
+        visited[nowX][nowY] = bit;
+        if (ans == 26) {
+            return;
+        }
         for (int i = 0; i < 4; i++) {
             int nextX = nowX + dx[i];
             int nextY = nowY + dy[i];
             if (nextX < 0 || nextX >= R || nextY < 0 || nextY >= C) {
                 continue;
             }
-            if (visited[arr[nextX][nextY] - 'A']) {
+            /* 현재 문자를 이미 탐색한 경우 */
+            if ((bit & 1 << arr[nextX][nextY] - 'A') != 0) {
                 continue;
             }
-            dfs(nextX, nextY, cnt + 1);
+            /* 현재까지 방문했던 상태가 동일한 경우 */
+            if ((bit | 1 << arr[nextX][nextY] - 'A') == visited[nextX][nextY]) {
+                continue;
+            }
+            dfs(nextX, nextY, cnt + 1, bit | 1 << arr[nextX][nextY] - 'A');
         }
-        visited[arr[nowX][nowY] - 'A'] = false;
     }
 }
