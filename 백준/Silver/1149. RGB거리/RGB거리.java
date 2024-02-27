ceca4eebefa05d9eabcc34f90e46b238b19451a4
Jonghan Sim <simhani1@gmail.com>
2024-02-27 11:53:15 +0900

    [Silver I] Title: RGB거리, Time: 92 ms, Memory: 12108 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+    private static int N;
+    private static int[][] arr;
+    private static int[][] dp;
+
+    public static void main(String[] args) throws Exception {
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N + 1][3];
+        dp = new int[N + 1][3];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][2] = Integer.parseInt(st.nextToken());
+        }
+        dp[0][0] = arr[0][0];
+        dp[0][1] = arr[0][1];
+        dp[0][2] = arr[0][2];
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < 3; i++) {
+            ans = Math.min(ans, dp[N - 1][i]);
+        }
+        System.out.println(ans);
+    }
+}
