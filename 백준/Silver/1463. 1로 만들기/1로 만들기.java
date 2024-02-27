248eee82793fdc5d09732d09fcff908011ddbec0
Jonghan Sim <simhani1@gmail.com>
2024-02-27 22:05:48 +0900

    [Silver III] Title: 1로 만들기, Time: 108 ms, Memory: 15880 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -3,6 +3,27 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * @author
+ * 심종한
+ * @date
+ * 2024-02-27
+ * @link
+ * https://www.acmicpc.net/problem/1463
+ * https://drive.google.com/file/d/1yDp2SgGChA7LypQ4iR9i5AtxEN0NxKdg/view?usp=sharing
+ * @keyword_solution
+ * 10 -> 5 -> 4 -> 2 -> 1  == 4번 연산
+ * 10 -> 9 -> 3 -> 1  == 3번 연산
+ * 어떤 연산을 먼저 하는 것이 최적의 방법인지 판단할 수 없음
+ * dp[i]: N을 i로 만들기 위한 최소 연산 횟수
+ * @input
+ * N <= 10^6
+ * @output
+ * @time_complex
+ * O(N)
+ * @perf
+ * 메모리: 15,876kb, 시간: 116ms
+ */
 public class Main {
 
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,12 +31,13 @@ public class Main {
     private static StringBuilder sb = new StringBuilder();
     private static int N;
     private static int[] dp;
+    private static final int INF = 123456789;
 
     public static void main(String[] args) throws Exception {
         N = Integer.parseInt(br.readLine());
         /* i를 만드는데 필요한 최소 연산 횟수 */
         dp = new int[N + 1];
-        Arrays.fill(dp, 123456789);
+        Arrays.fill(dp, INF);
         dp[N] = 0;
         for (int i = N; i >= 1; i--) {
             if (i % 3 == 0) {
