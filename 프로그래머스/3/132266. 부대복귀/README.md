4fd47ec66e41fa443ccffaf84480aedb1c6d9d42
Jonghan Sim <simhani1@gmail.com>
2025-03-04 02:28:58 +0900

    [level 3] Title: 부대복귀, Time: 129.95 ms, Memory: 171 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/README.md
+++ 프로그래머스/3/132266. 부대복귀/README.md
@@ -1,6 +1,6 @@
 # [level 3] 부대복귀 - 132266 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132266?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132266) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 04일 02:28:48
+2025년 03월 04일 02:28:56
 
 ### 문제 설명
 
--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -4,17 +4,17 @@ class Solution {
     
     private List<Integer>[] edge;
     private int[] dist;
-    private int[] answer;    
     private final int INF = Integer.MAX_VALUE;
-
+    
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        answer = new int[sources.length];
-        dist = new int[n + 1];
-        Arrays.fill(dist, INF);
+        int[] answer = new int[sources.length];
+        
         edge = new ArrayList[n + 1];
         for (int i = 0; i <= n; i++) {
             edge[i] = new ArrayList<>();
         }
+        dist = new int[n + 1];
+        Arrays.fill(dist, INF);
         
         for (int i = 0; i < roads.length; i++) {
             int a = roads[i][0];
@@ -23,21 +23,20 @@ class Solution {
             edge[b].add(a);
         }
         
-        bfs(destination);
         
-        for (int i = 0; i < sources.length; i++) {
-            answer[i] = dist[sources[i]] == INF ? -1 : dist[sources[i]];
-        }
+        bfs(sources[0], destination);
         
         return answer;
     }
     
-    private void bfs(int destination) {
+    private int bfs(int source, int destination) {
         Queue<Integer> q = new ArrayDeque<>();
-        q.offer(destination);
-        dist[destination] = 0;
+        q.offer(source);
         while(!q.isEmpty()) {
             int now = q.poll();
+            if (now == destination) {
+                break;
+            }
             for (int next : edge[now]) {
                 if (dist[next] == INF) {
                     dist[next] = dist[now] + 1;
@@ -45,5 +44,6 @@ class Solution {
                 }
             }
         }
+        return -1;
     }
 }
