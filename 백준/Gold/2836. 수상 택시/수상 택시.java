0a5b897cfa70d9aa5f63e213eff15893340a22e2
Jonghan Sim <simhani1@gmail.com>
2025-05-27 01:49:50 +0900

    [Gold III] Title: 수상 택시, Time: 652 ms, Memory: 103308 KB -BaekjoonHub

--- 백준/Gold/2836. 수상 택시/수상 택시.java
+++ 백준/Gold/2836. 수상 택시/수상 택시.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,9 +10,9 @@ public class Main {
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
     static int N, M;
-    static List<Taxi> list = new ArrayList<>();
+    static PriorityQueue<Taxi> pq = new PriorityQueue<>();
 
-    static class Taxi {
+    static class Taxi implements Comparable<Taxi> {
         int start;
         int end;
 
@@ -29,6 +28,14 @@ public class Main {
                 ", end=" + end +
                 '}';
         }
+
+        @Override
+        public int compareTo(Taxi o) {
+            if (this.end == o.end) {
+                return Integer.compare(this.start, o.start) * -1;
+            }
+            return Integer.compare(this.end, o.end);
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -39,35 +46,17 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            if (end < start) {
-                list.add(new Taxi(start, end));
-            }
+            if (start <= end) continue;
+            pq.offer(new Taxi(start, end));
         }
-        list.sort((o1, o2) -> {
-            if (o1.end == o2.end) {
-                return Integer.compare(o1.start, o2.start) * -1;
-            }
-            return Integer.compare(o1.end, o2.end);
-        });
         long ans = M;
-        if (list.size() == 0) {
-            System.out.println(ans);
-            return; 
-        }
-        int prevStart = list.get(0).start;
-        int prevEnd = list.get(0).end;
-        for (int i = 1; i < list.size(); i++) {
-            int curStart = list.get(i).start;
-            int curEnd = list.get(i).end;
-            if (curEnd <= prevStart) {
-                prevStart = Math.max(prevStart, curStart);
-            } else {
-                ans += Math.abs(prevEnd - prevStart) * 2L;
-                prevStart = curStart;
-                prevEnd = curEnd;
+        while (!pq.isEmpty()) {
+            Taxi now = pq.poll();
+            while (!pq.isEmpty() && pq.peek().end <= now.start) {
+                now.start = Math.max(now.start, pq.poll().start);
             }
+            ans += Math.abs(now.end - now.start) * 2L;
         }
-        ans += Math.abs(prevEnd - prevStart) * 2L;
         System.out.println(ans);
     }
 }
