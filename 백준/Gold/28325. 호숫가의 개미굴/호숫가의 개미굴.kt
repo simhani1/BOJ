b62a874443af85fcbb73bd668b7fef024421a107
Jonghan Sim <simhani1@gmail.com>
2025-06-15 01:57:27 +0900

    [Gold IV] Title: 호숫가의 개미굴, Time: 344 ms, Memory: 66092 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.kt
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.kt
@@ -1,21 +1,31 @@
-import kotlin.math.max
+import java.util.*
 
 val br = System.`in`.bufferedReader()
 val sb = StringBuilder()
 
 fun main() {
     val N = br.readLine().toInt()
-    val arr = br.readLine().split(" ").map { it.toLong() }.toLongArray()
-    val dp = Array(N + 1) { LongArray(2) }
-    dp[1][0] = 1L
-    dp[1][1] = arr[0]
-    for (i in 2..N) {
-        dp[i][0] = dp[i - 1][1] + 1
-        if (arr[i - 1] != 0L) {
-            dp[i][1] = max(dp[i - 1][0], dp[i - 1][1]) + arr[i - 1]
-        } else {
-            dp[i][1] = dp[i][0] - 1
+    val arr = LongArray(N)
+    val list = ArrayList<Int>()
+    val st = StringTokenizer(br.readLine())
+    var ans = 0L
+    for (i in 0 until N) {
+        arr[i] = st.nextToken().toLong()
+        if (arr[i] > 0) {
+            ans += arr[i]
+            list.add(i)
         }
     }
-    println(max(dp[N][0], dp[N][1]))
+    if (ans == 0L) {
+        println(N / 2)
+        return
+    }
+    for (i in 0 until list.size) {
+        if (i == list.size - 1) {
+            ans += (N - (list[i] - list[0])) / 2L
+        } else {
+            ans += (list[i + 1] - list[i]) / 2L
+        }
+    }
+    println(ans)
 }
