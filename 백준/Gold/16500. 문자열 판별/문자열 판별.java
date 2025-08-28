141584530021044ed940796c5d674aa461a21fd9
Jonghan Sim <simhani1@gmail.com>
2025-08-29 00:06:39 +0900

    [Gold V] Title: 문자열 판별, Time: 68 ms, Memory: 11596 KB -BaekjoonHub

---
+++ 백준/Gold/16500. 문자열 판별/문자열 판별.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static String str;
+	static int N;
+	static boolean[] visited;
+	static String[] arr;
+	static boolean flag = false;
+
+	public static void main(String[] args) throws IOException {
+		str = br.readLine();
+		N = Integer.parseInt(br.readLine());
+		arr = new String[N];
+		visited = new boolean[str.length()];
+		for (int i = 0; i < N; i++) {
+			arr[i] = br.readLine();
+		}
+		dfs(0);
+		System.out.println(flag ? 1 : 0);
+	}
+
+	private static void dfs(int idx) {
+		if (idx > str.length()) {
+			return;
+		}
+		if (idx == str.length()) {
+			flag = true;
+			return;
+		}
+		for (String word : arr) {
+			if (str.startsWith(word, idx) && !visited[idx + word.length() - 1]) {
+				visited[idx + word.length() - 1] = true;
+				dfs(idx + word.length());
+			}
+		}
+	}
+}
